0b4b88a1774e5d85db12b867982efab99a05a88e
George Tushinsky <george.tushinsky@gmail.com>
2021-01-25 11:19:36 +0200

    Added role and permissions for BookStore

--- src/Acme.BookStore.Application/Books/BookAppService.cs
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Acme.BookStore.Permissions;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -7,16 +8,21 @@ namespace Acme.BookStore.Books
 {
     public class BookAppService :
         CrudAppService<
-            Book,
-            BookDto,
-            Guid,
-            PagedAndSortedResultRequestDto,
-            CreateUpdateBookDto>,
-        IBookAppService
+            Book, //The Book entity
+            BookDto, //Used to show books
+            Guid, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto>, //Used to create/update a book
+        IBookAppService //implement the IBookAppService
     {
-        public BookAppService(IRepository<Book, Guid> repository) : base(repository)
+        public BookAppService(IRepository<Book, Guid> repository)
+            : base(repository)
         {
-
+            GetPolicyName = BookStorePermissions.Books.Default;
+            GetListPolicyName = BookStorePermissions.Books.Default;
+            CreatePolicyName = BookStorePermissions.Books.Create;
+            UpdatePolicyName = BookStorePermissions.Books.Edit;
+            DeletePolicyName = BookStorePermissions.Books.Delete;
         }
     }
 }
